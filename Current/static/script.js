df5ca64a9eda60039959ed3b76c337301aed4611
TDKien20020200 <20020200@vnu.edu.vn>
2024-01-24 12:01:01 +0700

    Completed version of Youtube

--- Current/static/script.js
+++ Current/static/script.js
@@ -4,25 +4,26 @@ var counter = 15;
 
 function startUpdateCycle() {
     updatePrices();
-    var countdown = setInterval(function () {
+    setInterval(function () {
         counter--;
-        $('#counter <= 0') {
+        $('#counter').text(counter);
+        if (counter <= 0) {
             updatePrices();
-            counter = 15
+            counter = 15;
         }
     }, 1000)
 }
 
 $(document).ready(function () {
     tickers.forEach(function(ticker) {
-        addTickerToGrid();
+        addTickerToGrid(ticker);
     });
 
     updatePrices();
 
     $('#add-ticker-form').submit(function(e) {
         e.preventDefault();
-        var newTicker = $('@new-ticker').val().toUpperCase();
+        var newTicker = $('#new-ticker').val().toUpperCase();
         if (!tickers.includes(newTicker)) {
             tickers.push(newTicker);
             localStorage.setItem('tickers', JSON.stringify(tickers))
@@ -36,10 +37,60 @@ $(document).ready(function () {
         var tickerToRemove = $(this).data('ticker');
         tickers = tickers.filter(t => t !== tickerToRemove);
         localStorage.setItem('tickers', JSON.stringify(tickers))
-        $('#${tickerToRemove}').remove();
+        $(`#${tickerToRemove}`).remove();
     });
 
     startUpdateCycle();
 })
 
-# m25
+
+function addTickerToGrid(ticker) {
+    $('#tickers-grid').append(`<div id="${ticker}" class="stock-box"><h2>${ticker}</h2><p id="${ticker}-price"></p><p id="${ticker}-pct"></p><button class="remove-btn" data-ticker="${ticker}">Remove</button></div>`)
+}
+
+function updatePrices() {
+    tickers.forEach(function (ticker) {
+        $.ajax({
+            url: '/get_stock_data',
+            type: 'POST',
+            data: JSON.stringify({'ticker': ticker}),
+            contentType: 'application/json; charset=utf-8',
+            dataType: 'json',
+            success: function (data) {
+                var changePercent = ((data.currentPrice - data.openPrice) / data.openPrice) * 100;
+                var colorClass;
+                if (changePercent <= -2) {
+                    colorClass = 'dark-red'
+                } else if (changePercent < 0) {
+                    colorClass = 'red'
+                } else if (changePercent == 0) {
+                    colorClass = 'gray'
+                } else if (changePercent <= 2) {
+                    colorClass = 'green'
+                } else {
+                    colorClass = 'dark-green'
+                }
+
+                $(`#${ticker}-price`).text(`$${data.currentPrice.toFixed(2)}`);
+                $(`#${ticker}-pct`).text(`${changePercent.toFixed(2)}%`);
+                $(`#${ticker}-price`).removeClass('dark-red red gray green dark-green').addClass(colorClass);
+                $(`#${ticker}-pct`).removeClass('dark-red red gray green dark-green').addClass(colorClass);
+
+                var flashClass;
+                if (lastPrices[ticker] > data.currentPrice) {
+                    flashClass = 'red-flash';
+                } else if (lastPrices[ticker] < data.currentPrice) {
+                    flashClass = 'green-flash';
+                } else {
+                    flashClass = 'gray-flash';
+                }
+                lastPrices[ticker] = data.currentPrice;
+
+                $(`#${ticker}`).addClass(flashClass);
+                setTimeout(function() {
+                    $(`#${ticker}`).removeClass(flashClass);
+                }, 1000);
+            }
+        })
+    })
+}

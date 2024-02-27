fcaa84dec5c852af8c3bff6274f1e94252a15abd
TDKien20020200 <20020200@vnu.edu.vn>
2024-02-28 00:31:46 +0700

    Nothing important

--- Current/app.py
+++ Current/app.py
@@ -2,6 +2,7 @@ import yfinance as yf
 from flask import request, render_template, jsonify, Flask
 import matplotlib.pyplot as plt
 import matplotlib
+
 matplotlib.use('Agg')
 from torchsummary import summary
 from sklearn.preprocessing import MinMaxScaler
@@ -13,6 +14,60 @@ import numpy as np
 import pandas as pd
 
 app = Flask(__name__, template_folder='templates')
+
+# Xây dựng model Xác định kiến trúc: vì là vấn đề về chuỗi thời gian nên sẽ sử dụng Long Short-term Memory (LSTM) để nắm
+# bắt thông tin tuần tự:
+class NeuralNetwork(nn.Module):
+    def __init__(self, num_feature):
+        super(NeuralNetwork, self).__init__()
+        self.lstm = nn.LSTM(num_feature, 64, batch_first=True)
+        self.fc = nn.Linear(64, num_feature)
+
+    def forward(self, x):
+        output, (hidden, cell) = self.lstm(x)
+        x = self.fc(hidden)
+        return x
+
+
+# #############################################################################################################
+model = NeuralNetwork(4)
+# push to cuda if available
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model = model.to(device)
+# Hàm Optimize Adam
+optimizer = optim.Adam(model.parameters())
+# Hàm Loss MSELoss
+mse = nn.MSELoss()
+
+# Mô hình Training: xác định quá trình thuận và nghịch (forward and backward) để train mạng lưới Neural:
+def train(dataloader):
+    epoch_loss = 0
+    model.train()
+
+    for batch in dataloader:
+        optimizer.zero_grad()
+        x, y = batch
+        pred = model(x)
+        loss = mse(pred[0], y)
+        loss.backward()
+        optimizer.step()
+        epoch_loss += loss.item()
+
+    return epoch_loss
+
+# Đánh giá hiệu suất mô hình
+def evaluate(dataloader):
+    epoch_loss = 0
+    model.eval()
+
+    with torch.no_grad():
+        for batch in dataloader:
+            x, y = batch
+            pred = model(x)
+            loss = mse(pred[0], y)
+            epoch_loss += loss.item()
+
+    return epoch_loss / len(dataloader)
 
 @app.route('/')
 def index():
@@ -41,119 +96,84 @@ def ticker_detail(ticker):
     # Weekly Chart
     data.asfreq('W', method='ffill').plot(subplots=True, figsize=(14.5, 6.5), style='-')
     plt.savefig('./static/image/weekly_chart.png')
-    
+
     # Calculate MAs
     ma_day = [10, 20, 50]
     for ma in ma_day:
         col_name = f'MA for {ma} days'
         data[col_name] = data['Close'].rolling(ma).mean()
     data[['Close', 'MA for 10 days', 'MA for 20 days', 'MA for 50 days']].plot(figsize=(14.5, 6.5))
-    plt.title('Comparision some MA and Close')
+    plt.title('Comparison of Moving Averages and Close')
     plt.savefig('./static/image/MAs_chart.png')
-    
+
     # Dùng hàm pct_change() để tìm phần trăm thay đổi của giá Close mỗi ngày
-    data['Daily_Return'] = df['Close'].pct_change()
+    data['Daily_Return'] = data['Close'].pct_change()
     data.Daily_Return.plot(legend=True, figsize=(14.5, 6.5))
-    plt.title('Daily return percentage')
+    plt.title('Daily Return Percentage')
     plt.savefig('./static/image/daily_return_chart.png')
-    
+
     ###############################################################################################################
     # Tạo dataset theo batch size trong pytorch
     # Chuẩn hóa dữ liệu
     data2 = data.copy(deep=True)
-    scaler = MinMaxScaler(feature_range=(0, 15)).fit(df2.Low.values.reshape(-1, 1))
-    data2['Open'] = scaler.transform(data2.Open.values.reshape(-1, 1))
-    data2['High'] = scaler.transform(data2.High.values.reshape(-1, 1))
-    data2['Low'] = scaler.transform(data2.Low.values.reshape(-1, 1))
-    data2['Close'] = scaler.transform(data2.Close.values.reshape(-1, 1))
-    normalizationData = data2[['Open', 'High', 'Low', 'Close']].values
-    # chuẩn bị dữ liệu cho bài toán dự đoán giá cổ phiếu dựa trên chuỗi thời gian. Nó tạo ra các chuỗi (sequences) dựa
-    # trên giá cổ phiếu từ 10 ngày trước để dự đoán giá cổ phiếu vào ngày tiếp theo
-    seq_len = 11
-    sequences = []
-    for index in range(len(normalizationData) - seq_len + 1):
-        sequences.append(normalizationData[index: index + seq_len])
-    sequences = np.array(sequences)
-    # Customize dataset Tách dữ liệu toàn bộ tập dữ liệu thành ba phần. 80% cho tập huấn luyện (train), 10% cho tập xác
-    # thực (valid) và 10% còn lại cho tập kiểm thử (test):
-    valid_set_size_percentage = 10
-    test_set_size_percentage = 10
+    scaler = MinMaxScaler()
+    data2['Close'] = scaler.fit_transform(data2['Close'].values.reshape(-1, 1))
+    data2['Open'] = scaler.transform(data2['Open'].values.reshape(-1, 1))
+    data2['High'] = scaler.transform(data2['High'].values.reshape(-1, 1))
+    data2['Low'] = scaler.transform(data2['Low'].values.reshape(-1, 1))
 
-    valid_set_size = int(np.round(valid_set_size_percentage / 100 * sequences.shape[0]))
-    test_set_size = int(np.round(test_set_size_percentage / 100 * sequences.shape[0]))
-    train_set_size = sequences.shape[0] - (valid_set_size + test_set_size)
+    # Tạo dữ liệu huấn luyện và kiểm tra
+    train_data = data2.iloc[:-30]
+    test_data = data2.iloc[-30:]
 
-    x_train = sequences[:train_set_size, :-1, :]
-    y_train = sequences[:train_set_size, -1, :]
+    # Chuyển đổi dữ liệu thành dạng tensor
+    train_tensor = torch.tensor(train_data[['Close', 'Open', 'High', 'Low']].values).float()
+    test_tensor = torch.tensor(test_data[['Close', 'Open', 'High', 'Low']].values).float()
 
-    x_valid = sequences[train_set_size:train_set_size + valid_set_size, :-1, :]
-    y_valid = sequences[train_set_size:train_set_size + valid_set_size, -1, :]
+    # Tạo dataset và dataloader
+    train_dataset = TensorDataset(train_tensor[:-1], train_tensor[1:])
+    test_dataset = TensorDataset(test_tensor[:-1], test_tensor[1:])
+    train_dataloader = DataLoader(train_dataset, batch_size=32, shuffle=True)
+    test_dataloader = DataLoader(test_dataset, batch_size=32, shuffle=False)
 
-    x_test = sequences[train_set_size + test_set_size:, :-1, :]
-    y_test = sequences[train_set_size + test_set_size:, -1, :]
-    # DataLoader
-    # Tạo Trình tải dữ liệu: xác định các trình tải dữ liệu để tải tập dữ liệu theo từng batch với batch size = 32
-    x_train = torch.tensor(x_train).float()
-    y_train = torch.tensor(y_train).float()
+    # Huấn luyện mô hình
+    num_epochs = 100
+    train_losses = []
+    test_losses = []
 
-    x_valid = torch.tensor(x_valid).float()
-    y_valid = torch.tensor(y_valid).float()
-
-    train_dataset = TensorDataset(x_train, y_train)
-    train_dataloader = DataLoader(train_dataset, batch_size=32, shuffle=False)
-
-    valid_dataset = TensorDataset(x_valid, y_valid)
-    valid_dataloader = DataLoader(valid_dataset, batch_size=32, shuffle=False)
-    # ############################################################################################################# 
-    model = NeuralNetwork(4)
-    # push to cuda if available
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    model = model.to(device)
-    # Hàm Optimize Adam
-    optimizer = optim.Adam(model.parameters())
-    # Hàm Loss MSELoss
-    mse = nn.MSELoss()
-    # Train mô hình trong 50 data sau đó sẽ lưu mô hình tốt nhất trong quá trình đào tạo dựa trên việc xác định Giá trị
-    # bị mất (Val Loss):
-    n_epochs = 100
-    best_valid_loss = float('inf')
-    for epoch in range(1, n_epochs + 1):
+    for epoch in range(num_epochs):
         train_loss = train(train_dataloader)
-        valid_loss = evaluate(valid_dataloader)
-        # save the best model
-        if valid_loss < best_valid_loss:
-            best_valid_loss = valid_loss
-            torch.save(model, 'saved_weights.pt')
-        # print("Epoch ",epoch+1)
-        # print(f'\tTrain Loss: {train_loss:.5f} | ' + f'\tVal Loss: {valid_loss:.5f}\n')
-    # Từ kết quả trên suy ra đuợc mô hình tốt nhất, từ đó đưa ra vài dự đoán ban đầu
-    model=torch.load('saved_weights.pt')
-    x_test= torch.tensor(x_test).float()
+        test_loss = evaluate(test_dataloader)
+        train_losses.append(train_loss)
+        test_losses.append(test_loss)
+
+        if epoch % 10 == 0:
+            print(f'Epoch {epoch+1}/{num_epochs}, Train Loss: {train_loss:.4f}, Test Loss: {test_loss:.4f}')
+
+    # Dự đoán giá đóng cửa
     with torch.no_grad():
-      y_test_pred = model(x_test)
-    y_test_pred = y_test_pred.numpy()[0]
-    # visualize các dự đoán và so sánh chúng với thực tế:
-    idx=0
-    plt.plot(np.arange(y_train.shape[0], y_train.shape[0]+y_test.shape[0]),
-             y_test[:,idx], color='black', label='test target')
-    
-    plt.plot(np.arange(y_train.shape[0], y_train.shape[0]+y_test_pred.shape[0]),
-             y_test_pred[:,idx], color='green', label='test prediction')
-    plt.title('future stock prices')
-    plt.xlabel('time [days]')
-    plt.ylabel('normalized price')
-    plt.legend(loc='best')
-    plt.savefig('./static/image/predict_visualization.png')
-    
-    
+        model.eval()
+        predicted = model(test_tensor[:-1].to(device)).cpu().numpy()
+        predicted = scaler.inverse_transform(predicted)
 
+    # Plot dự đoán và giá thực tế
+    plt.figure(figsize=(14.5, 6.5))
+    x_values = test_data.index[1:len(predicted) + 1]
+    y_values_actual = test_data['Close'].values[1:len(predicted) + 1]
+    y_values_predicted = predicted.flatten()
 
+    plt.plot(x_values, y_values_actual, label='Actual Close Price')
+    plt.plot(x_values, y_values_predicted[:len(x_values)], label='Predicted Close Price')
+    plt.title('Actual Close Price vs Predicted Close Price')
+    plt.xlabel('Date')
+    plt.ylabel('Close Price')
+    plt.legend()
+    plt.savefig('./static/image/prediction_chart.png')
 
-    data = data.tail(30)
-    data_list = data.reset_index().to_dict(orient='records')
-    
-    return render_template('ticker_detail.html', ticker=ticker, current_price=current_price, open_price=open_price,
-                           volume=volume, data_list=data_list, chart_data=chart_data)
+    # Truyền dữ liệu cho template
+    return render_template('ticker.html', ticker=ticker, current_price=current_price, open_price=open_price,
+                           volume=volume, chart_data=chart_data, train_losses=train_losses, test_losses=test_losses)
+
 
 @app.route('/get_stock_data', methods=['POST'])
 def get_stock_data():
@@ -162,49 +182,6 @@ def get_stock_data():
     return jsonify({'currentPrice': data.iloc[-1].Close,
                     'openPrice': data.iloc[-1].Open})
 
-# Xây dựng model Xác định kiến trúc: vì là vấn đề về chuỗi thời gian nên sẽ sử dụng Long Short-term Memory (LSTM) để nắm
-# bắt thông tin tuần tự:
-class NeuralNetwork(nn.Module):
-        def __init__(self, num_feature):
-            super(NeuralNetwork, self).__init__()
-            self.lstm = nn.LSTM(num_feature, 64, batch_first=True)
-            self.fc = nn.Linear(64, num_feature)
-
-        def forward(self, x):
-            output, (hidden, cell) = self.lstm(x)
-            x = self.fc(hidden)
-            return x
-
- # Mô hình Training: xác định quá trình thuận và nghịch (forward and backward) để train mạng lưới Neural:
-def train(dataloader):
-    epoch_loss = 0
-    model.train()
-
-    for batch in dataloader:
-        optimizer.zero_grad()
-        x, y = batch
-        pred = model(x)
-        loss = mse(pred[0], y)
-        loss.backward()
-        optimizer.step()
-        epoch_loss += loss.item()
-
-    return epoch_loss
-
-
-# Đánh giá hiệt suất mô hình
-def evaluate(dataloader):
-    epoch_loss = 0
-    model.eval()
-
-    with torch.no_grad():
-        for batch in dataloader:
-            x, y = batch
-            pred = model(x)
-            loss = mse(pred[0], y)
-            epoch_loss += loss.item()
-
-    return epoch_loss / len(dataloader)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- Current/python/LSTM1.py
+++ Current/python/LSTM1.py
@@ -88,164 +88,164 @@ df.insert(0, 'Close', col_close)
 # # plt.title('Daily return percentage of Google stock')
 # # plt.show()
 #
-# ###############################################################################################################
-# # Tạo dataset theo batch size trong pytorch
-# # Chuẩn hóa dữ liệu
-# df2 = df.copy(deep=True)
-# scaler = MinMaxScaler(feature_range=(0, 15)).fit(df2.Low.values.reshape(-1, 1))
-# df2['Open'] = scaler.transform(df2.Open.values.reshape(-1, 1))
-# df2['High'] = scaler.transform(df2.High.values.reshape(-1, 1))
-# df2['Low'] = scaler.transform(df2.Low.values.reshape(-1, 1))
-# df2['Close'] = scaler.transform(df2.Close.values.reshape(-1, 1))
-# data = df2[['Open', 'High', 'Low', 'Close']].values
-# # print(data.shape)
-# # print(data)
-#
-#
-# # chuẩn bị dữ liệu cho bài toán dự đoán giá cổ phiếu dựa trên chuỗi thời gian. Nó tạo ra các chuỗi (sequences) dựa
-# # trên giá cổ phiếu từ 10 ngày trước để dự đoán giá cổ phiếu vào ngày tiếp theo
-# seq_len = 11
-# sequences = []
-# for index in range(len(data) - seq_len + 1):
-#     sequences.append(data[index: index + seq_len])
-# sequences = np.array(sequences)
+###############################################################################################################
+# Tạo dataset theo batch size trong pytorch
+# Chuẩn hóa dữ liệu
+df2 = df.copy(deep=True)
+scaler = MinMaxScaler(feature_range=(0, 15)).fit(df2.Low.values.reshape(-1, 1))
+df2['Open'] = scaler.transform(df2.Open.values.reshape(-1, 1))
+df2['High'] = scaler.transform(df2.High.values.reshape(-1, 1))
+df2['Low'] = scaler.transform(df2.Low.values.reshape(-1, 1))
+df2['Close'] = scaler.transform(df2.Close.values.reshape(-1, 1))
+data = df2[['Open', 'High', 'Low', 'Close']].values
+# print(data.shape)
+# print(data)
 
 
-# # Customize dataset Tách dữ liệu toàn bộ tập dữ liệu thành ba phần. 80% cho tập huấn luyện (train), 10% cho tập xác
-# # thực (valid) và 10% còn lại cho tập kiểm thử (test):
-# valid_set_size_percentage = 10
-# test_set_size_percentage = 10
-
-# valid_set_size = int(np.round(valid_set_size_percentage / 100 * sequences.shape[0]))
-# test_set_size = int(np.round(test_set_size_percentage / 100 * sequences.shape[0]))
-# train_set_size = sequences.shape[0] - (valid_set_size + test_set_size)
-
-# x_train = sequences[:train_set_size, :-1, :]
-# y_train = sequences[:train_set_size, -1, :]
-
-# x_valid = sequences[train_set_size:train_set_size + valid_set_size, :-1, :]
-# y_valid = sequences[train_set_size:train_set_size + valid_set_size, -1, :]
-
-# x_test = sequences[train_set_size + test_set_size:, :-1, :]
-# y_test = sequences[train_set_size + test_set_size:, -1, :]
+# chuẩn bị dữ liệu cho bài toán dự đoán giá cổ phiếu dựa trên chuỗi thời gian. Nó tạo ra các chuỗi (sequences) dựa
+# trên giá cổ phiếu từ 10 ngày trước để dự đoán giá cổ phiếu vào ngày tiếp theo
+seq_len = 11
+sequences = []
+for index in range(len(data) - seq_len + 1):
+    sequences.append(data[index: index + seq_len])
+sequences = np.array(sequences)
 
 
-# # DataLoader
-# # Tạo Trình tải dữ liệu: xác định các trình tải dữ liệu để tải tập dữ liệu theo từng batch với batch size = 32
-# x_train = torch.tensor(x_train).float()
-# y_train = torch.tensor(y_train).float()
+# Customize dataset Tách dữ liệu toàn bộ tập dữ liệu thành ba phần. 80% cho tập huấn luyện (train), 10% cho tập xác
+# thực (valid) và 10% còn lại cho tập kiểm thử (test):
+valid_set_size_percentage = 10
+test_set_size_percentage = 10
 
-# x_valid = torch.tensor(x_valid).float()
-# y_valid = torch.tensor(y_valid).float()
+valid_set_size = int(np.round(valid_set_size_percentage / 100 * sequences.shape[0]))
+test_set_size = int(np.round(test_set_size_percentage / 100 * sequences.shape[0]))
+train_set_size = sequences.shape[0] - (valid_set_size + test_set_size)
 
-# train_dataset = TensorDataset(x_train, y_train)
-# train_dataloader = DataLoader(train_dataset, batch_size=32, shuffle=False)
+x_train = sequences[:train_set_size, :-1, :]
+y_train = sequences[:train_set_size, -1, :]
 
-# valid_dataset = TensorDataset(x_valid, y_valid)
-# valid_dataloader = DataLoader(valid_dataset, batch_size=32, shuffle=False)
+x_valid = sequences[train_set_size:train_set_size + valid_set_size, :-1, :]
+y_valid = sequences[train_set_size:train_set_size + valid_set_size, -1, :]
 
-# # ############################################################################################################## Xây
-# # dựng model Xác định kiến trúc: vì là vấn đề về chuỗi thời gian nên sẽ sử dụng Long Short-term Memory (LSTM) để nắm
-# # bắt thông tin tuần tự:
-# class NeuralNetwork(nn.Module):
-#     def __init__(self, num_feature):
-#         super(NeuralNetwork, self).__init__()
-#         self.lstm = nn.LSTM(num_feature, 64, batch_first=True)
-#         self.fc = nn.Linear(64, num_feature)
-#
-#     def forward(self, x):
-#         output, (hidden, cell) = self.lstm(x)
-#         x = self.fc(hidden)
-#         return x
-#
-#
-# model = NeuralNetwork(4)
-#
-# # push to cuda if available
-# device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-# model = model.to(device)
-#
-# # summary(model, (4,))
-#
-# # Hàm Optimize Adam
-# optimizer = optim.Adam(model.parameters())
-# # Hàm Loss MSELoss
-# mse = nn.MSELoss()
-#
-#
-# # Mô hình Training: xác định quá trình thuận và nghịch (forward and backward) để train mạng lưới Neural:
-# def train(dataloader):
-#     epoch_loss = 0
-#     model.train()
-#
-#     for batch in dataloader:
-#         optimizer.zero_grad()
-#         x, y = batch
-#         pred = model(x)
-#         loss = mse(pred[0], y)
-#         loss.backward()
-#         optimizer.step()
-#         epoch_loss += loss.item()
-#
-#     return epoch_loss
-#
-#
-# # Đánh giá hiệt suất mô hình
-# def evaluate(dataloader):
-#     epoch_loss = 0
-#     model.eval()
-#
-#     with torch.no_grad():
-#         for batch in dataloader:
-#             x, y = batch
-#             pred = model(x)
-#             loss = mse(pred[0], y)
-#             epoch_loss += loss.item()
-#
-#     return epoch_loss / len(dataloader)
-#
-#
-# # Train mô hình trong 50 data sau đó sẽ lưu mô hình tốt nhất trong quá trình đào tạo dựa trên việc xác định Giá trị
-# # bị mất (Val Loss):
-# n_epochs = 100
-# best_valid_loss = float('inf')
-#
-# for epoch in range(1, n_epochs + 1):
-#
-#     train_loss = train(train_dataloader)
-#     valid_loss = evaluate(valid_dataloader)
-#
-#     # save the best model
-#     if valid_loss < best_valid_loss:
-#         best_valid_loss = valid_loss
-#         torch.save(model, 'saved_weights.pt')
-#
-#    # print("Epoch ",epoch+1)
-#    # print(f'\tTrain Loss: {train_loss:.5f} | ' + f'\tVal Loss: {valid_loss:.5f}\n')
-#
-# # Từ kết quả trên suy ra đuợc mô hình tốt nhất, từ đó đưa ra vài dự đoán ban đầu
-# model=torch.load('saved_weights.pt')
-#
-# x_test= torch.tensor(x_test).float()
-#
-# with torch.no_grad():
-#   y_test_pred = model(x_test)
-#
-# y_test_pred = y_test_pred.numpy()[0]
-# # visualize các dự đoán và so sánh chúng với thực tế:
-# idx=0
-# plt.plot(np.arange(y_train.shape[0], y_train.shape[0]+y_test.shape[0]),
-#          y_test[:,idx], color='black', label='test target')
-#
-# plt.plot(np.arange(y_train.shape[0], y_train.shape[0]+y_test_pred.shape[0]),
-#          y_test_pred[:,idx], color='green', label='test prediction')
-#
-# plt.title('future stock prices')
-# plt.xlabel('time [days]')
-# plt.ylabel('normalized price')
-# plt.legend(loc='best')
-# plt.show()
-#
+x_test = sequences[train_set_size + test_set_size:, :-1, :]
+y_test = sequences[train_set_size + test_set_size:, -1, :]
+
+
+# DataLoader
+# Tạo Trình tải dữ liệu: xác định các trình tải dữ liệu để tải tập dữ liệu theo từng batch với batch size = 32
+x_train = torch.tensor(x_train).float()
+y_train = torch.tensor(y_train).float()
+
+x_valid = torch.tensor(x_valid).float()
+y_valid = torch.tensor(y_valid).float()
+
+train_dataset = TensorDataset(x_train, y_train)
+train_dataloader = DataLoader(train_dataset, batch_size=32, shuffle=False)
+
+valid_dataset = TensorDataset(x_valid, y_valid)
+valid_dataloader = DataLoader(valid_dataset, batch_size=32, shuffle=False)
+
+# ############################################################################################################## Xây
+# dựng model Xác định kiến trúc: vì là vấn đề về chuỗi thời gian nên sẽ sử dụng Long Short-term Memory (LSTM) để nắm
+# bắt thông tin tuần tự:
+class NeuralNetwork(nn.Module):
+    def __init__(self, num_feature):
+        super(NeuralNetwork, self).__init__()
+        self.lstm = nn.LSTM(num_feature, 64, batch_first=True)
+        self.fc = nn.Linear(64, num_feature)
+
+    def forward(self, x):
+        output, (hidden, cell) = self.lstm(x)
+        x = self.fc(hidden)
+        return x
+
+
+model = NeuralNetwork(4)
+
+# push to cuda if available
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model = model.to(device)
+
+# summary(model, (4,))
+
+# Hàm Optimize Adam
+optimizer = optim.Adam(model.parameters())
+# Hàm Loss MSELoss
+mse = nn.MSELoss()
+
+
+# Mô hình Training: xác định quá trình thuận và nghịch (forward and backward) để train mạng lưới Neural:
+def train(dataloader):
+    epoch_loss = 0
+    model.train()
+
+    for batch in dataloader:
+        optimizer.zero_grad()
+        x, y = batch
+        pred = model(x)
+        loss = mse(pred[0], y)
+        loss.backward()
+        optimizer.step()
+        epoch_loss += loss.item()
+
+    return epoch_loss
+
+
+# Đánh giá hiệt suất mô hình
+def evaluate(dataloader):
+    epoch_loss = 0
+    model.eval()
+
+    with torch.no_grad():
+        for batch in dataloader:
+            x, y = batch
+            pred = model(x)
+            loss = mse(pred[0], y)
+            epoch_loss += loss.item()
+
+    return epoch_loss / len(dataloader)
+
+
+# Train mô hình trong 50 data sau đó sẽ lưu mô hình tốt nhất trong quá trình đào tạo dựa trên việc xác định Giá trị
+# bị mất (Val Loss):
+n_epochs = 100
+best_valid_loss = float('inf')
+
+for epoch in range(1, n_epochs + 1):
+
+    train_loss = train(train_dataloader)
+    valid_loss = evaluate(valid_dataloader)
+
+    # save the best model
+    if valid_loss < best_valid_loss:
+        best_valid_loss = valid_loss
+        torch.save(model, 'saved_weights.pt')
+
+   # print("Epoch ",epoch+1)
+   # print(f'\tTrain Loss: {train_loss:.5f} | ' + f'\tVal Loss: {valid_loss:.5f}\n')
+
+# Từ kết quả trên suy ra đuợc mô hình tốt nhất, từ đó đưa ra vài dự đoán ban đầu
+model=torch.load('saved_weights.pt')
+
+x_test= torch.tensor(x_test).float()
+
+with torch.no_grad():
+  y_test_pred = model(x_test)
+
+y_test_pred = y_test_pred.numpy()[0]
+# visualize các dự đoán và so sánh chúng với thực tế:
+idx=0
+plt.plot(np.arange(y_train.shape[0], y_train.shape[0]+y_test.shape[0]),
+         y_test[:,idx], color='black', label='test target')
+
+plt.plot(np.arange(y_train.shape[0], y_train.shape[0]+y_test_pred.shape[0]),
+         y_test_pred[:,idx], color='green', label='test prediction')
+
+plt.title('future stock prices')
+plt.xlabel('time [days]')
+plt.ylabel('normalized price')
+plt.legend(loc='best')
+plt.show()
+
 # #######################################################################################################3
 # index_values = df[len(df) - len(y_test):].index
 # col_values = ['Open', 'Low', 'High', 'Close']

2abc894e49c25b998ed5eb3918e1efb612812650
TDKien20020200 <20020200@vnu.edu.vn>
2024-05-06 17:26:03 +0700

    some changes

--- Current/complexVersions/testdata.py
+++ Current/complexVersions/testdata.py
@@ -10,18 +10,5 @@ print("dataYears:")
 print(dataYears.shape)
 print(dataYears)
 
-data2 = dataYears.copy(deep=True)
-print("data2:")
-print(data2.shape)
-print(data2)
-
-scaler = MinMaxScaler(feature_range=(0, 15))
-data2[['Close', 'Open', 'High', 'Low']] = scaler.fit_transform(data2[['Close', 'Open', 'High', 'Low']])
-print("After data2:")
-print(data2.shape)
-print(data2)
-
-dataUse = data2[['Open', 'High', 'Low', 'Close']].values
-print("dataUse:")
-print(dataUse.shape)
-print(dataUse)
+with open('saved_weights.pt', 'w') as f:
+    pass
